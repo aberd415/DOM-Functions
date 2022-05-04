ba1a5065a1ea750e91caa058274db315ca12ee56
aberd415 <aberd415@hsd.k12.or.us>
2022-05-04 15:27:11 -0700

    Additional java mouse tracking

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
-// Test Scripts
-alert("Did this work?");
+
+var paragraph = document.createElement(paragraph);
+
+paragraph.textContent = 'The DOM is the bomb!';
+
+document.body.appendChild(paragraph);
+
+
+var div = document.createElement('DIV');
+
+div.style.height = '100vh';
+
+document.body.appendChild(div);
+
+div.addEventListener('mousemove', function(event) {
+    console.log(event);
+    var x = event.clientX;
+    var y = event.clientY;
+    div.textContent = x + ', ' + y;
+    div.style.backgroundColor = 'rgb('+ x + ', ' + y + ', 100)';
+});
